1d08775989099669cec06139b051474a9ac5d2ea
Rafiwawawa <rafi.aditiya.ariyadi@gmail.com>
2025-06-16 15:48:19 +0700

    Update AdminPromoController.php

--- AdminPromoController.php
+++ AdminPromoController.php
@@ -17,17 +17,6 @@ class AdminPromoController extends Controller {
         $this->promoModel = $this->loadModel('Promo');
     }
 
-    public function index() {
-        $promos = $this->promoModel->getAllPromos();
-        $this->loadView('promo/index', [
-            'title' => 'Kelola Promo',
-            'promos' => $promos,
-            'success' => $_SESSION['success'] ?? null,
-            'error' => $_SESSION['error'] ?? null
-        ]);
-        unset($_SESSION['success'], $_SESSION['error']);
-    }
-
     public function createform() {
         $this->loadView('promo/create', ['title' => 'Tambah Promo']);
     }
