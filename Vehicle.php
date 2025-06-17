b0f834cb9dcb25776c88d671ee64d76b27732e7a
kusn18 <esnarois4@gmail.com>
2025-06-17 12:57:40 +0700

    Update Vehicle.php

--- Vehicle.php
+++ Vehicle.php
@@ -2,16 +2,22 @@
 require_once 'Model.php'; 
 
 class Vehicle extends Model {
-    public function getAll() {
+    public function getAll($isAdmin = false) {
         $conn = $this->getConnection();
-        $query = "SELECT id_vehicle, jenis_kendaraan, merk, detail_kendaraan, kota, harga_per_hari, gambar_url FROM vehicle ORDER BY id_vehicle DESC";
-        $result = mysqli_query($conn, $query); // Jalankan query
-        // mengembalikan data yg bs diulang di view jika getAll dipanggil
+        $query = "SELECT id_vehicle, jenis_kendaraan, merk, detail_kendaraan, kota, harga_per_hari, gambar_url, is_aktif FROM vehicle";
+
+        if (!$isAdmin) {
+            $query .= " WHERE is_aktif = 1"; // User biasa hanya lihat yang aktif
+        }
+
+        $query .= " ORDER BY id_vehicle DESC";
+        
+        $result = mysqli_query($conn, $query);
         $data = [];
         if ($result) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $data[] = $row;
-          }
+            while ($row = mysqli_fetch_assoc($result)) {
+                $data[] = $row;
+            }
         }
         return $data;
     }
@@ -19,32 +25,23 @@ class Vehicle extends Model {
     public function getFilteredVehicles($kota, $tanggalMulai, $waktuMulai, $tanggalSelesai, $waktuSelesai) {
     $conn = $this->getConnection();
 
-    $query = "SELECT id_vehicle, jenis_kendaraan, merk, detail_kendaraan, kota, harga_per_hari, gambar_url FROM vehicle WHERE kota = ?"; 
+    $query = "SELECT id_vehicle, jenis_kendaraan, merk, detail_kendaraan, kota, harga_per_hari, gambar_url, is_aktif FROM vehicle WHERE kota = ? AND is_aktif = 1"; 
     $stmt = mysqli_prepare($conn, $query);
-
     mysqli_stmt_bind_param($stmt, "s", $kota); // 's' untuk string (kota)
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
 
     $data = [];
     while ($row = mysqli_fetch_assoc($result)) {
-        $data[] = [
-            'id_vehicle' => $row['id_vehicle'],
-            'jenis_kendaraan' => $row['jenis_kendaraan'],
-            'merk' => $row['merk'],
-            'detail_kendaraan' => $row['detail_kendaraan'],
-            'kota' => $row['kota'],
-            'harga_per_hari' => (float)$row['harga_per_hari'],
-            'gambar_url' => $row['gambar_url'] ?? 'style/assets/default.png'
-        ];
+        $data[] = $row;
     }
     mysqli_stmt_close($stmt);
     return $data;
     }
 
-        public function getById($id) {
+    public function getById($id) {
         $conn = $this->getConnection();
-        $query = "SELECT id_vehicle, jenis_kendaraan, merk, detail_kendaraan, kota, harga_per_hari, gambar_url FROM vehicle WHERE id_vehicle = ?";
+        $query = "SELECT id_vehicle, jenis_kendaraan, merk, detail_kendaraan, kota, harga_per_hari, gambar_url, is_aktif FROM vehicle WHERE id_vehicle = ?";
         $stmt = mysqli_prepare($conn, $query);
         mysqli_stmt_bind_param($stmt, "i", $id); // 'i' for integer (id_vehicle)
         mysqli_stmt_execute($stmt);
@@ -53,9 +50,10 @@ class Vehicle extends Model {
         mysqli_stmt_close($stmt);
         return $vehicle;
     }
-     public function create($data) {
+
+    public function create($data) {
         $conn = $this->getConnection();
-        $query = "INSERT INTO vehicle (jenis_kendaraan, merk, detail_kendaraan, kota, harga_per_hari, gambar_url) VALUES (?, ?, ?, ?, ?, ?)";
+        $query = "INSERT INTO vehicle (jenis_kendaraan, merk, detail_kendaraan, kota, harga_per_hari, gambar_url, is_aktif) VALUES (?, ?, ?, ?, ?, ?, ?)";
         
         $stmt = mysqli_prepare($conn, $query);
         mysqli_stmt_bind_param($stmt, "ssssds", 
@@ -64,7 +62,8 @@ class Vehicle extends Model {
             $data['detail_kendaraan'], 
             $data['kota'], 
             $data['harga_per_hari'], 
-            $data['gambar_url']
+            $data['gambar_url'],
+            $data['is_aktif']
         );
 
         return mysqli_stmt_execute($stmt);
@@ -72,7 +71,7 @@ class Vehicle extends Model {
 
     public function update($id, $data) {
         $conn = $this->getConnection();
-        $query = "UPDATE vehicle SET jenis_kendaraan = ?, merk = ?, detail_kendaraan = ?, kota = ?, harga_per_hari = ?, gambar_url = ? WHERE id_vehicle = ?";
+        $query = "UPDATE vehicle SET jenis_kendaraan = ?, merk = ?, detail_kendaraan = ?, kota = ?, harga_per_hari = ?, gambar_url = ?, is_aktif = ? WHERE id_vehicle = ?";
         
         $stmt = mysqli_prepare($conn, $query);
         mysqli_stmt_bind_param($stmt, "ssssdsi", 
@@ -82,8 +81,19 @@ class Vehicle extends Model {
             $data['kota'], 
             $data['harga_per_hari'], 
             $data['gambar_url'],
+            $data['is_aktif'],
             $id
         );
+
+        return mysqli_stmt_execute($stmt);
+    }
+
+    public function toggleStatus($id) {
+        $conn = $this->getConnection();
+        $query = "UPDATE vehicle SET is_aktif = NOT is_aktif WHERE id_vehicle = ?";
+        
+        $stmt = mysqli_prepare($conn, $query);
+        mysqli_stmt_bind_param($stmt, "i", $id);
 
         return mysqli_stmt_execute($stmt);
     }
